a325e803665bf8c6cfc0ef0d1a9c3e5b0d2b476e
Valerie Graham <vgraham@bayviewglen.ca>
2024-06-06 18:27:20 -0400

    Puzzles outline start

--- src/zork/Puzzles.java
+++ src/zork/Puzzles.java
@@ -2,6 +2,14 @@ package zork;
 
 public class Puzzles {
     public static void SnowcatManualPuzzle() {
+/* 5 puzzles total: battery, fuel can, wires, find missing tire, engine ?
+  chest object - player must unscramble a phrase "All work and no play makes Jack a dull boy" which is found in a drawer in Jack's desk
+  they must input it to open the chest, giving them 1/5 puzzles to fix the snowcat
+  battery and fuel can - fuel can is located in the garage, player must find the battery first and somehow those two are related and "unlock" together
+  fuel can is frozen in a block of ice, player must carry it inside and wait for it to melt
+  wires are located next to telephone
 
+  these puzzles are active during the "escape the hotel" and non-insane runthrough, where Jack is supposed to solve all puzzles and escape via snowcat
+*/
     }
 }
