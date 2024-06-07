e86b05986d84523b0b8f54a83533c4b4de25cf16
Joline Lian <jlian2@bayviewglen.ca>
2024-06-07 14:22:19 -0400

    removed

--- src/zork/Puzzles.java
+++ src/zork/Puzzles.java
@@ -3,15 +3,6 @@ package zork;
 import java.util.*;
 
 public class Puzzles {
-    /* 5 puzzles total: battery, fuel can, wires, find missing tire, engine ?
-    chest object - player must unscramble a phrase "All work and no play makes Jack a dull boy" which is found in a drawer in Jack's desk
-    they must input it to open the chest, giving them 1/5 puzzles to fix the snowcat
-    battery and fuel can - fuel can is located in the garage, player must find the battery first and somehow those two are related and "unlock" together
-    fuel can is frozen in a block of ice, player must carry it inside and wait for it to melt
-    wires are located next to telephone
-
-    these puzzles are active during the "escape the hotel" and non-insane runthrough, where Jack is supposed to solve all puzzles and escape via snowcat
-    */
     public static boolean manualPuzzleChest() {
         Scanner in = new Scanner(System.in);
         
@@ -44,6 +35,12 @@ public class Puzzles {
         if (playersKey.equals(manualKey)) {
             return true;
         }
+
+        return false;
+    }
+
+    public static boolean tirePuzzle() {
+
 
         return false;
     }
